b039211818a3298f5ec51c3d19f8202907599795
James Martin <jjamesmartiin@gmail.com>
2025-04-18 02:14:56 -0700

    something basic working for recording and playing audio

--- shell.nix
+++ shell.nix
@@ -5,11 +5,16 @@ let
   pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cf8cc1201be8bc71b7cbbbdaf349b22f4f99c7ae.tar.gz") {};
 in pkgs.mkShell {
   packages = [
+    # other non-Python packages can be added here
+    pkgs.lame # for mp3 encoding
+
+    # Python packages
     (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
       # select Python packages here
       pyaudio
       openai-whisper
       keyboard
+      wavefile
     ]))
   ];
 }
